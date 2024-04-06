a8ef25a85e91449389703895e3ec799c90fcbe2d
Armia <armia.iiarmia@gmail.com>
2024-04-06 15:55:39 +0330

    this is app.js page for useing pakages and routes

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/admin', adminRouter);
+app.use(shopRouter);
+
+
+mongoose.connect('mongodb://localhost/Shop')
+    .then(result => {
+        app.listen(3000, () => {
+            console.log('Listening on port 3000');
+        });
+        
+    })
+    .catch(
+        err=>{
+            console.log(err);
+        }
+    )
