dd30238f236307add9bebd620dadd6da60a3095f
Armia <armia.iiarmia@gmail.com>
2024-04-06 18:22:03 +0330

    when made action on controllers after that in this file make routes for actions and use

---
+++ routes/shop.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+const shopController=require('../controllers/shop');
+
+
+
+router.get('/',shopController.getIndex);
+
+router.get('/products',shopController.getProducts);
+
+router.get('/products/:productId',shopController.getProduct);
+
+
+
+module.exports = router;
