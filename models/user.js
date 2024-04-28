3dfaf8736f637e7076400af86522cd54e210f93d
Armia <armia.iiarmia@gmail.com>
2024-04-28 14:17:21 +0330

    i`m writing a method in model with mongoose elements

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const createApplication = require('express/lib/express');
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -29,6 +30,28 @@ const userSchema = new Schema({
             }
         }]
     }
-})
+});
+
+userSchema.methods.addTocart = (product)=>{
+    const CartProductIndex = this.cart.items.findIndex(cp=>{
+        return cp.productId.toString() === product._id.toString()
+    })
+    let newQuantity = 1;
+    const updateCartItems = [...this.cart.items];
+    if(CartProductIndex >= 0){
+        newQuantity = this.cart.items[CartProductIndex].quantity + 1;
+        updateCartItems[CartProductIndex].quantity=newQuantity
+    }else{
+        updateCartItems.push({
+            productId:product._id,
+            quantity:newQuantity
+        })
+    }
+    const updateCart = {
+        items:updateCartItems
+    };
+    this.cart = updateCart;
+    return this.Save()
+}
 
 module.exports = mongoose.model('User',userSchema)
