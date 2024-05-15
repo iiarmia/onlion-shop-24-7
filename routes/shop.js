9af35b828992124644904f19ccf0200589119fa2
Armia <armia.iiarmia@gmail.com>
2024-05-15 14:43:41 +0330

    the end of that its ordrt router for delete order

--- routes/shop.js
+++ routes/shop.js
@@ -7,16 +7,17 @@ const shopController=require('../controllers/shop');
 
 
 
-router.get('/',shopController.getIndex);
+router.get('/', shopController.getIndex);
 
-router.get('/products',shopController.getProducts);
+router.get('/products', shopController.getProducts);
 
-router.get('/products/:productId',shopController.getProduct);
+router.get('/products/:productId', shopController.getProduct);
 
-router.post('/cart',shopController.postCart)
+router.post('/cart', shopController.postCart);
 
-router.get('/cart',shopController.getcart)
+router.get('/cart', shopController.getCart);
 
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 
 module.exports = router;
