6fa9e47e8cd86c3963f09b7972cbdac4df620881
Armia <armia.iiarmia@gmail.com>
2024-06-26 23:21:16 +0330

    testing for deBug

--- models/order.js
+++ models/order.js
@@ -1,34 +1,35 @@
-const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
-const { required } = require('nodemon/lib/config');
-const schema = mongoose.Schema();
 
-const orderSchma = new schema({
-    products:[
-        {
-            product:{
-             Type:Object,
-             required:true
-            },
-            quantity:{
-             Type:Number,
-             required:true
-            }
-        }
-    ],
+const Schema = mongoose.Schema;
 
-    user:
-      {
-        name:{
-            type:String,
-            required:true
+
+const orderSchema = new Schema({
+
+    products: [{
+        product: {
+            type: Object,
+            required: true
         },
-        UserId:{
-            type:this.Schema.Types.ObjectId,
-            required:true,
-            ref:'User'
+        quantity: {
+            type: Number,
+            required: true
         }
-      },
-})
+    }],
+    user: {
+        name: {
+            type: String,
+            required: true
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        }
+    }
 
-module.exports = mongoose,model('order', orderSchma )
+
+
+});
+
+
+module.exports = mongoose.model('Order', orderSchema);
