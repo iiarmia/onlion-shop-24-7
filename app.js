90c2cfa14eabbce63cd604ae31d343db0c70d677
Armia <armia.iiarmia@gmail.com>
2024-07-22 10:53:34 +0330

    right psision middailware for session id

--- app.js
+++ app.js
@@ -41,7 +41,10 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 
  app.use((req,res,next)=>{
-    User.findById('6629203517dd4a62202cb730').then(user=>{
+    if(!req.session.user){
+        return next()
+    }
+    User.findById(req.session.user._id).then(user=>{
         req.user=user;
         next()
     }).catch(err=>{
