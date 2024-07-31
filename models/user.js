51df79aafda8da8ea8f0a2df57fcae47fded226e
Armia <armia.iiarmia@gmail.com>
2024-08-01 01:58:00 +0330

    delete name on modele

--- models/user.js
+++ models/user.js
@@ -4,19 +4,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true,
-        minlength:3,
-        maxlength:15
-    },
+
     email:{
         type:String,
         required:true,
         unique:true,
         lowercase:true
     },
+
+    password:{
+        type:String,
+        required:true
+    }, 
+
     cart:{
         items:[{
             productId:{
