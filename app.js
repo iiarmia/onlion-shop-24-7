48199c3afa7ba4fa31d6b8e90e0b451c8a14f196
Armia <armia.iiarmia@gmail.com>
2024-08-04 15:50:11 +0330

    add flash package and add app.js file & make middleware consept for using other file or field

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
+const flash = require('connect-flash')
 
 
 const User = require('./models/user');
@@ -40,6 +41,7 @@ app.use(session({
     store: store
 }));
 app.use(csrfProtection);
+app.use(flash())
 app.use((req, res, next) => {
     if (!req.session.user) {
         return next();
